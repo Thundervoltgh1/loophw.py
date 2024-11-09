d0123e78a61f31d9c34d76ea074c4ad6d0b79e5d
Thundervoltgh1 <eniolusi@gmail.com>
2024-11-09 14:59:30 +0000

    Update loophw.py

--- loophw.py
+++ loophw.py
@@ -3,39 +3,73 @@ pygame.init()
 sw=800
 sh=600
 score=0
+speed=5
 flying=False
 screen= pygame.display.set_mode((sw,sh))
 pygame.display.set_caption("FLAPPY BIRD")
 screen.fill("white")
 bg=pygame.image.load('fbg.png')
 ground=pygame.image.load('fbground.png')
+pipe_freq=1500
 
 game_over=False
 fps=50
 font=pygame.font.SysFont('Bauhaus 93',60)
-
+lastpipe= pygame.time.get_ticks()-pipe_freq
 flying=False
 pygame.display.update()
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self,x,y,position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.image.load("pipe.png")
+        self.rect=self.image.get_rect()
+        if position==1:
+            self.image=pygame.transform.flip(self.image,False,True)
+            self.rect.bottomleft=[x,y]
+        elif position==-1:
+            self.rect.topleft=[x,y]
+    def update(self):
+        self.rect.x-=speed
+        
+pipegroup=pygame.sprite.Group()
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
         self.images=[]
         self.counter=0
-        self.index=0
+        self.index=0 
+        self.vel=0
         for i in range(1,4):
-            img=pygame.image.load(f"b{i}.png")
+            img=pygame.image.load(f"b{i}-removebg-preview.png")
             self.images.append(img)
         self.image=self.images[self.index]
         self.rect=self.image.get_rect()
         self.rect.center=[x,y]
+        self.clicked=False
     def update(self):
+        
         if game_over==False:
             self.counter+=1
             self.index+=1
+            if pygame.mouse.get_pressed()[0]==1 and self.clicked==False:
+                self.clicked=True
+                self.vel=-10
+            if pygame.mouse.get_pressed()[0]==0:
+                self.clicked=False
+        
             if self.index>=len(self.images):
                 self.index=0
             self.image=self.images[self.index]
+    
+        if flying== True:
+            self.vel+=0.5
+            if self.vel>8:
+                self.vel=8
+            if self.rect.bottom<550:   
+                self.rect.y+=self.vel
+
 
 flappy=Bird(100,sh/2)
 birdgroup=pygame.sprite.Group()
@@ -51,17 +85,30 @@ while True:
     clock.tick(fps)
     screen.blit(bg,(0,0))
     birdgroup.draw(screen)
+    pipegroup.draw(screen)
     screen.blit(ground,(ground_scroll,550))
     draw_text(str(score),10,10)
     ground_scroll-=ground_speed
     if abs(ground_scroll)>35:
         ground_scroll=0
 
+    if flying==True and game_over==False:
+        time_now=pygame.time.get_ticks()
+        if time_now-lastpipe>pipe_freq:
+            
+            bpipe=Pipe(sw,sh//2,-1)
+            pipegroup.add(bpipe)
+            tpipe=Pipe(sw,sh//2,1)
+            pipegroup.add(tpipe)
+            lastpipe=time_now
 
     birdgroup.update()
-    pygame.display.update()
+    pipegroup.update()
+    
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
         if event.type==pygame.MOUSEBUTTONDOWN and flying==False and game_over==False:
             flying=True
+ 
+    pygame.display.update()
